0fe79f6437e336edecdad10e09530cfa8efac6ae
Sayed <sayed.sais@gmail.com>
2024-08-19 11:34:17 +0100

    added header styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,8 +6,8 @@ export default function Header () {
 return (
 <header className ={styles.headerWrapper}>
     <img src={reactLogo} alt="React logo" className={styles.logo} />
-    <h1>React Flashcards</h1>   
-    <p>🧠Expand Your React Knowledge, One Flashcard at a Time!🐌</p>
+    <h1 className={styles.header}>React Flashcards</h1>   
+    <p className={styles.parag}>🧠Expand Your React Knowledge, One Flashcard at a Time!🐌</p>
 </header>
 )
 }
